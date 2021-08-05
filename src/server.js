26f982cc1191e9a85c8e2ed712db912baed80196
Rois Faozi <roisfaozi55@gmail.com>
2021-08-05 20:36:35 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+
+// music
 const music = require('./api/music')
 const MusicService = require('./services/MusicServices')
 const musicValidator = require('./validator/music')
 
+// users
+const users = require('./api/users')
+const UsersService = require('./services/postgres/UserService')
+const UsersValidator = require('./validator/users')
+
 const init = async () => {
   const musicService = new MusicService()
+  const usersService = new UsersService()
+
   const server = Hapi.server({
 
     port: process.env.PORT,
@@ -18,13 +27,22 @@ const init = async () => {
     }
   })
 
-  await server.register({
-    plugin: music,
-    options: {
-      service: musicService,
-      validator: musicValidator
+  await server.register([
+    {
+      plugin: music,
+      options: {
+        service: musicService,
+        validator: musicValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
     }
-  })
+  ])
 
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
